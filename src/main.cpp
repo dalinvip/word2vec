de8d2bf82228d31c0cd5fca2231adcfa5dee0a33
bamtercelboo <bamtercelboo@163.com>
2018-03-15 14:38:34 +0800

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @Author: bamtercelboo
+ * @Date： 2018/03/15
+ * @File: main.cpp
+ * @Contact： bamtercelboo@{gmail.com, 163.com}
+ * @Function：None
+ */
+ 
+#include<iostream>
+ 
+
+int main(int argc, char** argv){
+	std::cout << "word2vec" << std::endl;
+	std::getchar();
+	return 0;
+}
