5a57256710396288237e097c4e5e22d631c7afff
bamtercelboo <bamtercelboo@163.com>
2018-03-27 19:18:37 +0800

    extract_zh_char_stoke

---
+++ py_script/extract_zh_char_stoke/extract_zh_char_stoke.py
@@ -0,0 +1,21 @@
+# @Author : bamtercelboo
+# @Datetime : 2018/3/27 9:47
+# @File : extract_zh_char_stoke.py
+# @Last Modify Time : 2018/3/27 9:47
+# @Contact : bamtercelboo@{gmail.com, 163.com}
+
+"""
+    FILE :  extract_zh_char_stoke.py
+    FUNCTION : None
+    EXAMPLE:
+        Source:
+        中
+        TO:
+        中: 丨フ一丨
+"""
+
+
+
+if __name__ == "__main__":
+    print("extract chinese character stoke")
+
