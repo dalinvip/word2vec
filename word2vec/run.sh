40abadf4a82c1ae0f2e694da22a61c16c6e3bd81
bamtercelboo <bamtercelboo@163.com>
2018-03-31 16:10:35 +0800

    sample and run script

--- word2vec/run.sh
+++ word2vec/run.sh
@@ -1,4 +1,13 @@
 
-./Word2Vec skipgram -input /data/mszhang/ACL2017-Word2Vec/data/enwiki-20150112_text.txt -output context/enwiki.emb -lr 0.025 -dim 100  -ws 5 -epoch 5 -minCount 100 -neg 5 -loss ns  -minn 3 -maxn 6 -thread 12 -t 1e-4 -lrUpdateRate 100 > context.log 2>&1
+cp -rf ./bin/word2vec .
 
+path_input=./sample
+path_out=./sample
+path_inradical=./sample
+path_incomponent=./sample
 
+nohup ./word2vec skipgram -input ${path_input}/giga_char_sample.txt -output ${path_out}/giga_skipgram.300d -lr 0.025 -dim 300 -ws 5 -epoch 10 -minCount 10 -neg 5 -loss ns -thread 8 -t 1e-4 -lrUpdateRate 100 > log_skipgram_300d 2>&1 &
+
+nohup ./word2vec subradical -input ${path_input}/giga_char_sample.txt -inradical ${path_inradical}/char_radical_sample.txt -output ${path_out}/giga_subradical.300d -lr 0.025 -dim 300 -ws 5 -epoch 10 -minCount 10 -neg 5 -loss ns -thread 8 -t 1e-4 -lrUpdateRate 100 > log_subradical_300d 2>&1 &
+
+nohup ./word2vec subcomponent -input ${path_input}/giga_char_sample.txt -incomponent ${path_incomponent}/char_component_sample.txt -output ${path_out}/giga_subcomponent.300d -lr 0.025 -dim 300 -ws 5 -epoch 10 -minCount 10 -neg 5 -loss ns -thread 8 -t 1e-4 -lrUpdateRate 100 > log_subcomponent_300d 2>&1 &
