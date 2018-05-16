7f82efafd25a1972aa31a72da99c927e3bb77659
bamtercelboo <bamtercelboo@163.com>
2018-05-16 20:38:35 +0800

    delete script

--- py_script/extract_zh_char_radical/run.sh
+++
@@ -1,2 +0,0 @@
-nohup python3 -u extract_zh_char_radical.py --input ./Data/giga_small.txt --output ./Data/giga_small_out.txt --save_radical ./Data/radical > log_extract_radical 2>&1 &
-tail -f log_extract_radical
--- py_script/split_zh_char/run.sh
+++
@@ -1,2 +0,0 @@
-python -u split_zh_char.py --input ./Data/giga_small.txt --output ./Data/test.txt
-
